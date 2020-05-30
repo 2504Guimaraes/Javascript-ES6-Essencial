fa9c91b6d515f429dc05b6c5f7f4fac8385662bb
Ivan Guimarães dos Santos da Silva <guimaraes.ivan@hotmail.com>
2020-05-30 17:37:58 -0300

    Renaming file

---
+++ Aulas/Currying.js
@@ -0,0 +1,34 @@
+/*
+    Currying é o processo de transformar uma função que espera vários 
+    argumentos em uma função que espera um único argumento e retorna 
+    outra função curried. 
+    
+    Uma função que normalmente recebe 3 argumentos, ao sofrer currying, 
+    resulta em uma função que recebe um argumento e retorna uma função 
+    que recebe um argumento, que por sua vez retorna uma função que 
+    recebe um argumento e retorna o resultado da função original.
+*/
+
+/* Função Primeiro Grau sem Currying */
+
+function primeiroGrau(a,b,x) {
+  return a * x + b;
+}
+
+console.log(primeiroGrau(3,2,42));
+
+
+/* Função Primeiro Grau com Currying */
+
+function primeiroGrauCurried(a) { 
+    return function(b) {
+      return function(x) {
+  
+       return a * x + b;
+      };
+    };
+  }
+  
+  let resultado = primeiroGrauCurried(3)(2)(42);
+  
+  console.log(resultado);
