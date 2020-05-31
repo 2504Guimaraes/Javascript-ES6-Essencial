cf91a8c30c3aa6006797fe6b8a5b8280b7a56a11
Ivan Guimarães dos Santos da Silva <guimaraes.ivan@hotmail.com>
2020-05-31 20:14:11 -0300

    Create tiposDeVaviaveis.js

---
+++ Aulas/tiposDeVaviaveis.js
@@ -0,0 +1,45 @@
+/*
+
+    Javascript antigamente só tinha escopo de função e 
+    de bloco, com a adição de Let e Const agora foi
+    adicionado o escopo de bloco.
+
+    Vat não obedece escopode bloco, mas obedece escopo
+    de função.
+
+    Let e Const obedecem escopo de bloco, e const é
+    um valor que possui atribuição imutável.
+
+    Abaixo um exemplo de como VAR não respeita escopo
+    de bloco, dará um undefined mostrando q a variável
+    msm não atribuida existe dando undefined, em vez de um erro.
+
+*/
+
+/* Teste com var: */
+
+(function(){
+    
+    console.log(`valor = ${test}`);
+
+    if (true) {
+        var test = 21; 
+        console.log(`valor dentro de if= ${test}`);   
+    }
+
+    console.log(`valor após if = ${test}`);
+})()
+
+/* Teste com let (tbm funciona com const): */
+
+(function(){
+    
+    console.log(`valor = ${test}`);
+
+    if (true) {
+        let test = 21; 
+        console.log(`valor dentro de if= ${test}`);   
+    }
+
+    console.log(`valor após if = ${test}`);
+})()
